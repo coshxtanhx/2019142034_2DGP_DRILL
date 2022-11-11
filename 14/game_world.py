80f5aa19e3f74242751128f0b967dc1a28c88f2b
Cho_YongJoon <yzz1108j@gmail.com>
2022-11-12 00:13:37 +0900

    lec 14 added
    2

---
+++ 14/game_world.py
@@ -0,0 +1,79 @@
+
+# layer 0: Background Objects
+# layer 1: Foreground Objects
+objects = [[], []]
+
+#collision information
+#key: string 'boy:balls'
+#value: [[boy], [ball1, ball2, ...]]
+collision_group = dict()
+
+def add_object(o, depth):
+    objects[depth].append(o)
+
+def add_objects(ol, depth):
+    objects[depth] += ol
+
+def add_collision_pairs(a, b, group):
+    if group not in collision_group:
+        print('add new group')
+        collision_group[group] = [[], []]
+
+    if a:
+        if type(a) == list:
+            collision_group[group][0] += a
+        else:
+            collision_group[group][0].append(a)
+    if b:
+        if type(b) == list:
+            collision_group[group][1] += b
+        else:
+            collision_group[group][1].append(b)
+
+def all_collision_pairs():
+    for group, pairs in collision_group.items(): #key value 가져오기
+        for a in pairs[0]:
+            for b in pairs[1]:
+                yield a, b, group
+
+def remove_collision_object(o):
+    for pairs in collision_group.values(): #키는 필요 없음
+        if o in pairs[0]: pairs[0].remove(o)
+        elif o in pairs[1]: pairs[1].remove(o)
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            #충돌 그룹에서도 지우기
+            remove_collision_object(o)
+            del o
+            return
+    raise ValueError('Trying destroy non existing object')
+
+
+# def remove_object(o):
+#     for layer in objects:
+#         try:
+#             layer.remove(o)
+#             del o
+#             return
+#         except:
+#             pass
+#     raise ValueError('Trying destroy non existing object')
+
+
+def all_objects():
+    for layer in objects:
+        for o in layer:
+            yield o
+
+
+def clear():
+    for o in all_objects():
+        del o
+    for layer in objects:
+        layer.clear()
+
+
+
